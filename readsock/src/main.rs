41694fdac423d36dafb404d828d4c046100825ec
Jeff Biseda <jbiseda@gmail.com>
2022-02-07 17:15:44 -0800

    x

--- readsock/src/main.rs
+++ readsock/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
+use std::net::UdpSocket;
+
+
+fn main() -> std::io::Result<()> {
     println!("Hello, world!");
+
+    let socket = UdpSocket::bind("127.0.0.1:2345")?;
+
+    let mut pktcnt = 0;
+
+    while true {
+        let mut buf = [0; 1500];
+        let (amt, src) = socket.recv_from(&mut buf)?;
+        println!("pkt len={} cnt={}", amt, pktcnt);
+        pktcnt += 1;
+    }
+    
+    
+    Ok(())
 }
--- writesock/src/main.rs
+++ writesock/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
+use std::net::{UdpSocket, SocketAddr};
+
+fn main() -> std::io::Result<()> {
     println!("Hello, world!");
+
+    let socket = UdpSocket::bind("127.0.0.1:0")?;
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 2345));
+
+    let mut buf = [5; 999];
+
+    for i in 0..1000 {
+        socket.send_to(&buf, &addr)?;
+        std::thread::sleep(std::time::Duration::from_millis(1_000));
+        println!("sent pktno={}", i);
+    }
+
+    Ok(())
 }
