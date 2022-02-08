b4a22eda80ae30326c369ef738b96b16bd46ded4
Jeff Biseda <jbiseda@gmail.com>
2022-02-08 03:44:36 -0800

    add dos option

--- writesock/src/main.rs
+++ writesock/src/main.rs
@@ -1,11 +1,39 @@
 use std::env;
 use std::net::{UdpSocket, SocketAddr};
 
+
+fn dos(addr_string: String) -> std::io::Result<()> {
+
+    println!("DOS...");
+
+    let socket = UdpSocket::bind("0.0.0.0:0")?;
+
+    let addr: SocketAddr = addr_string.parse().unwrap();
+
+    let buf = [5; 999];
+
+    for i in 0..1_000 {
+        for _ in 0..1_000 {
+            socket.send_to(&buf, &addr)?;
+        }
+        println!("sent pktno={}", i);
+        std::thread::sleep(std::time::Duration::from_millis(1_000));
+    }
+
+    Ok(())
+}
+
+
 fn main() -> std::io::Result<()> {
 
     let args: Vec<String> = env::args().collect();
 
     let addr_string = &args[1];
+
+    if args.len() > 2 {
+        dos(addr_string.to_string())?;
+        return Ok(());
+    }
 
     println!("Sending...");
 
@@ -15,7 +43,7 @@ fn main() -> std::io::Result<()> {
 
     let buf = [5; 999];
 
-    for i in 0..1000 {
+    for i in 0..1_000 {
         socket.send_to(&buf, &addr)?;
         std::thread::sleep(std::time::Duration::from_millis(1_000));
         println!("sent pktno={}", i);
