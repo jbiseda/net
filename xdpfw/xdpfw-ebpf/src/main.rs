4b25d6a150be9675404733435c9ae01f5fdde120
Jeff Biseda <jbiseda@gmail.com>
2022-02-18 15:59:54 -0800

    x

--- xdpfw/xdpfw-ebpf/src/main.rs
+++ xdpfw/xdpfw-ebpf/src/main.rs
@@ -99,7 +99,7 @@ unsafe fn try_xdpfw(ctx: XdpContext) -> Result<u32, ()> {
     let mut pkt_count = 0;
     match VARS.get(&VAR_PACKET_COUNT) {
         Some(ptr) => {
-            let val = ptr as *mut u64;
+            let val = ptr as *const _ as *const _ as *mut u64;
             *val += 1;
             pkt_count = *val;
         },
